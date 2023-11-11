b3caa89ed88f00d8cacb9f297f68335f988ba48c
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-11-10 18:41:06 -0600

    feat: Navbar and Home components created, font downloaded

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <img src="https://1000marcas.net/wp-content/uploads/2019/11/Logo-Nike-1.png" width={70} alt="nike__logo" className="logo" />
+        <ul className="navbar__menu">
+            <li>MUJERES</li>
+            <li>NIÑOS</li>
+            <li>HOMBRES</li>
+        </ul>
+        <div className="navbar__user-actions">
+        </div>
+        .
+    </nav>
+  );
+}
+
+export default Navbar;
