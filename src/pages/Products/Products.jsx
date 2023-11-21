bead27b9872866f9ddd012fd78299e26fc448fae
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-11-20 18:32:43 -0600

    Add dynamic rendering of products in Products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Products.module.css";
 import TenisCard from "./components/TenisCard";
 function Products() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([])
+  useEffect(()=>{
+    async function getProducts(){
+      const response = await fetch('http://127.0.0.1:5000/products')
+      const data = await response.json()
+      setIsLoading(false)
+      setProducts(data[0].productos)
+      console.log(data[0].productos)
+    }
+
+    getProducts()
+  },[])
   return (
     <>
       <div className="wrapper">
@@ -9,9 +23,11 @@ function Products() {
       </div>
       <div className="wrapper">
         <div className={styles.products__grid}>
-          <TenisCard />
-          <TenisCard />
-          <TenisCard />
+          {
+            isLoading ? <p>Loading...</p> : products.map((product,index)=>
+               (<TenisCard key={index} img={product.productImage} name={product.productName} price={product.productPrice} />)
+            ) 
+          }
         </div>
       </div>
     </>
--- src/pages/Products/components/TenisCard.jsx
+++ src/pages/Products/components/TenisCard.jsx
@@ -1,11 +1,11 @@
 import styles from "./TenisCard.module.css";
-function TenisCard() {
+function TenisCard({img, name, price}) {
   return (
     <div className={styles.tenis__card}>
-      <img src="/public/images/Products page/tenis.png" alt="tenis" />
+      <img src={img} alt="tenis__image" />
       <p className={styles.tenis__status}>Recien llegados</p>
-      <p className={styles.tenis__name}>Nike Air Force 1 PLT.AF.ORM</p>
-      <p className={styles.tenis__price}>$1999,99</p>
+      <p className={styles.tenis__name}>{name}</p>
+      <p className={styles.tenis__price}>{price}</p>
     </div>
   );
 }
